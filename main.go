045395f5730cb42cfc260c345009610c93774912
Max Houston Ramirez Martel <8385004+soyHouston256@users.noreply.github.com>
2024-04-09 01:37:32 -0500

    feat: add create products

--- main.go
+++ main.go
@@ -8,6 +8,23 @@ import (
 func main() {
 	driver := storage.Mysql
 	storage.New(driver)
+	m := model.Product{
+		Name:  "Php Course",
+		Price: 100,
+	}
+	m2 := model.Product{
+		Name:  "Python Course",
+		Price: 100,
+	}
+	obs := "This is a golang course"
+	m3 := model.Product{
+		Name:        "Python Course",
+		Price:       100,
+		Observation: &obs,
+	}
 
-	storage.DB().AutoMigrate(&model.Product{}, &model.InvoiceHeader{}, &model.InvoiceItem{})
+	storage.DB().Create(&m)
+	storage.DB().Create(&m2)
+	storage.DB().Create(&m3)
+
 }
