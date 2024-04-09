26cd5424d312ecff2a2d26600412583c9dac64fe
Max Houston Ramirez Martel <8385004+soyHouston256@users.noreply.github.com>
2024-04-09 01:43:33 -0500

    feat: get all products

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/soyhouston256/go-gorm/model"
 	"github.com/soyhouston256/go-gorm/storage"
 )
@@ -8,23 +10,12 @@ import (
 func main() {
 	driver := storage.Mysql
 	storage.New(driver)
-	m := model.Product{
-		Name:  "Php Course",
-		Price: 100,
-	}
-	m2 := model.Product{
-		Name:  "Python Course",
-		Price: 100,
-	}
-	obs := "This is a golang course"
-	m3 := model.Product{
-		Name:        "Python Course",
-		Price:       100,
-		Observation: &obs,
-	}
 
-	storage.DB().Create(&m)
-	storage.DB().Create(&m2)
-	storage.DB().Create(&m3)
+	product := make([]model.Product, 0)
 
+	storage.DB().Find(&product)
+
+	for _, p := range product {
+		fmt.Printf("ID: %d - %s\n", p.ID, p.Name)
+	}
 }
